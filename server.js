db17fa3ba7dc14f0c25f0d7ad640d54ba1da521d
JayGodbole <godbolejay12345@gmail.com>
2026-06-23 11:32:00 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,48 +1,111 @@
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
-const io = require('socket.io')(http, {
-    cors: { origin: "*" }
-});
+const io = require('socket.io')(http);
+const fs = require('fs');
 const path = require('path');
 
 const PORT = process.env.PORT || 3000;
+const DATA_FILE = './players_database.json';
 
-// Serve the game files
 app.use(express.static(__dirname));
 
-// Global World State
-let players = {};
-let buildings = [];
+// Persistent database
+let database = { users: {} };
+if (fs.existsSync(DATA_FILE)) {
+    try {
+        database = JSON.parse(fs.readFileSync(DATA_FILE));
+    } catch (e) { console.error("DB Load Error", e); }
+}
+
+function saveDB() {
+    fs.writeFileSync(DATA_FILE, JSON.stringify(database, null, 2));
+}
+
+let activePlayers = {}; 
+let globalBuildings = []; 
 
 io.on('connection', (socket) => {
-    console.log('New survivor joined:', socket.id);
+    console.log('User connected:', socket.id);
 
-    // 1. Send current world to new player
-    socket.emit('init-world', { players, buildings });
+    socket.on('auth-request', (data) => {
+        const { username, password, type } = data;
+        
+        if (type === 'register') {
+            if (database.users[username]) {
+                socket.emit('auth-response', { success: false, msg: 'User already exists!' });
+            } else {
+                database.users[username] = {
+                    password,
+                    stats: { hp: 100, hunger: 100, thirst: 100, wood: 50 },
+                    inv: [5, 5, 5, 5],
+                    pos: { x: 0, y: 5, z: 0 },
+                    buildings: []
+                };
+                saveDB();
+                socket.emit('auth-response', { success: true, user: username, data: database.users[username] });
+            }
+        } else {
+            // Explicit Login
+            const user = database.users[username];
+            if (user) {
+                if (user.password === password) {
+                    socket.emit('auth-response', { success: true, user: username, data: user });
+                } else {
+                    socket.emit('auth-response', { success: false, msg: 'Wrong password!' });
+                }
+            } else {
+                socket.emit('auth-response', { success: false, msg: 'Account not found. Please register.' });
+            }
+        }
+    });
 
-    // 2. Handle Player Movement
-    socket.on('move', (data) => {
-        players[socket.id] = {
+    socket.on('join-game', (data) => {
+        activePlayers[socket.id] = {
             id: socket.id,
             name: data.name,
             pos: data.pos,
             rot: data.rot
         };
-        socket.broadcast.emit('player-moved', players[socket.id]);
+        socket.broadcast.emit('player-joined', activePlayers[socket.id]);
+        socket.emit('init-world', { 
+            players: activePlayers, 
+            buildings: globalBuildings 
+        });
     });
 
-    // 3. Handle Building
+    socket.on('move', (data) => {
+        if (activePlayers[socket.id]) {
+            activePlayers[socket.id].pos = data.pos;
+            activePlayers[socket.id].rot = data.rot;
+            socket.broadcast.emit('player-moved', { id: socket.id, pos: data.pos, rot: data.rot });
+        }
+    });
+
     socket.on('build', (buildData) => {
-        buildings.push(buildData);
+        globalBuildings.push(buildData);
         io.emit('new-build', buildData);
     });
 
-    // 4. Handle Disconnect
+    socket.on('interact', (targetId) => {
+        const initiator = activePlayers[socket.id];
+        if (initiator && activePlayers[targetId]) {
+            io.to(targetId).emit('interact-receive', { from: initiator.name });
+            socket.emit('interact-confirm', { to: activePlayers[targetId].name });
+        }
+    });
+
+    socket.on('save-progress', (data) => {
+        if (database.users[data.username]) {
+            database.users[data.username].stats = data.stats;
+            database.users[data.username].pos = data.pos;
+            saveDB();
+        }
+    });
+
     socket.on('disconnect', () => {
-        console.log('Survivor left:', socket.id);
-        delete players[socket.id];
-        io.emit('player-left', socket.id);
+        socket.broadcast.emit('player-left', socket.id);
+        delete activePlayers[socket.id];
     });
 });
 
