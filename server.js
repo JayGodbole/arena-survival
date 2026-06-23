c6203194ca37b576b684891f9cef3d20da8628fb
JayGodbole <godbolejay12345@gmail.com>
2026-06-23 13:24:57 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -14,41 +14,41 @@ let database = { users: {} };
 if (fs.existsSync(DATA_FILE)) {
     try {
         database = JSON.parse(fs.readFileSync(DATA_FILE));
-    } catch (e) { console.log("New database created."); }
+    } catch (e) { console.log("Init new DB"); }
 }
 
 function saveDB() {
     fs.writeFileSync(DATA_FILE, JSON.stringify(database, null, 2));
 }
 
-let players = {};
+let activePlayers = {}; 
 let worldBuildings = [];
 
 io.on('connection', (socket) => {
     socket.on('auth-request', (data) => {
         const { user, pass, type } = data;
         if (type === 'reg') {
-            if (database.users[user]) return socket.emit('auth-res', { success: false, msg: 'Name taken!' });
-            database.users[user] = { pass, stats: { hp: 100, wood: 50 }, pos: { x: 0, y: 5, z: 0 }, inv: [5, 5, 5, 5] };
+            if (database.users[user]) return socket.emit('auth-res', { success: false, msg: 'Account exists!' });
+            database.users[user] = { pass, stats: { hp: 100, wood: 50 }, pos: { x: 0, y: 5, z: 0 }, inv: [1, 5, 5, 5, 5] };
             saveDB();
             socket.emit('auth-res', { success: true, user, data: database.users[user] });
         } else {
             const entry = database.users[user];
             if (entry && entry.pass === pass) socket.emit('auth-res', { success: true, user, data: entry });
-            else socket.emit('auth-res', { success: false, msg: 'Invalid login!' });
+            else socket.emit('auth-res', { success: false, msg: 'Invalid Login' });
         }
     });
 
     socket.on('join', (data) => {
-        players[socket.id] = { id: socket.id, name: data.name, pos: data.pos, yaw: data.yaw };
-        socket.broadcast.emit('p-joined', players[socket.id]);
-        socket.emit('init-world', { players, buildings: worldBuildings });
+        activePlayers[socket.id] = { id: socket.id, name: data.name, pos: data.pos, yaw: data.yaw };
+        socket.broadcast.emit('p-joined', activePlayers[socket.id]);
+        socket.emit('init-world', { players: activePlayers, buildings: worldBuildings });
     });
 
     socket.on('move', (data) => {
-        if (players[socket.id]) {
-            players[socket.id].pos = data.pos;
-            players[socket.id].yaw = data.yaw;
+        if (activePlayers[socket.id]) {
+            activePlayers[socket.id].pos = data.pos;
+            activePlayers[socket.id].yaw = data.yaw;
             socket.broadcast.emit('p-moved', { id: socket.id, pos: data.pos, yaw: data.yaw });
         }
     });
@@ -59,13 +59,21 @@ io.on('connection', (socket) => {
     });
 
     socket.on('interact', (id) => {
-        if (players[id]) io.to(id).emit('p-interact', players[socket.id].name);
+        if (activePlayers[id]) io.to(id).emit('p-interact', activePlayers[socket.id].name);
+    });
+
+    socket.on('save-progress', (data) => {
+        if (database.users[data.user]) {
+            database.users[data.user].stats = data.stats;
+            database.users[data.user].pos = data.pos;
+            saveDB();
+        }
     });
 
     socket.on('disconnect', () => {
-        delete players[socket.id];
-        io.emit('p-left', socket.id);
+        socket.broadcast.emit('p-left', socket.id);
+        delete activePlayers[socket.id];
     });
 });
 
-http.listen(PORT, () => console.log('Server live at port ' + PORT));
+http.listen(PORT, () => console.log('Multiplayer Server live on port ' + PORT));
